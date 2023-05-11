a14bcbc4912bf2c62f97a8b4ec143b31fcacbbff
Dacian Avram~ <daciancatalin20@gmail.com~>
2023-05-11 16:18:41 +0300

    add guard for routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import "./app.css";
-    import Router from "svelte-spa-router";
-
+    import Router, { replace } from "svelte-spa-router";
+    import { wrap } from "svelte-spa-router/wrap";
     import ConnectCirclesSafe from "./routes/ConnectCirclesSafe.svelte";
     import ConnectWallet from "./pages/ConnectWallet.svelte";
     import CreateCirclesSafe from "./routes/CreateCirclesSafe.svelte";
@@ -9,16 +9,58 @@
     import MintHoG from "./routes/MintHoG.svelte";
     import Home from "./routes/Home.svelte";
     import TransferHog from "./routes/TransferHog.svelte";
+    import { selectedSafe } from "./stores/singletons/selectedSafe";
+    import { connectedWalletAddress } from "./stores/singletons/connectedWalletAddress";
 
     const routes = {
         "/": Home,
         "/connect-wallet": ConnectWallet,
-        "/connect-circles-safe": ConnectCirclesSafe,
-        "/import-circles-safe": ImportCirclesSafe,
-        "/create-circles-safe": CreateCirclesSafe,
-        "/mint-hog": MintHoG,
-        "/transfer-hog": TransferHog,
+        "/connect-circles-safe": wrap({
+            component: ConnectCirclesSafe,
+            conditions: [
+                () => {
+                    return $connectedWalletAddress ? true : false;
+                },
+            ],
+        }),
+        "/import-circles-safe": wrap({
+            component: ImportCirclesSafe,
+            conditions: [
+                () => {
+                    return $connectedWalletAddress ? true : false;
+                },
+            ],
+        }),
+        "/create-circles-safe": wrap({
+            component: CreateCirclesSafe,
+            conditions: [
+                () => {
+                    return $connectedWalletAddress ? true : false;
+                },
+            ],
+        }),
+        "/mint-hog": wrap({
+            component: MintHoG,
+            conditions: [
+                () => {
+                    return $selectedSafe ? true : false;
+                },
+            ],
+        }),
+        "/transfer-hog": wrap({
+            component: TransferHog,
+            conditions: [
+                () => {
+                    return $selectedSafe ? true : false;
+                },
+            ],
+        }),
     };
+
+    function conditionsFailed(event) {
+        console.error("conditionsFailed event", event.detail);
+        replace("/connect-wallet");
+    }
 </script>
 
-<Router {routes} />
+<Router {routes} on:conditionsFailed={conditionsFailed} />
